eeb9cde96e1e9ca3608a3288c5fc6bd1ae64763c
delisma <david.elisma@cds-snc.ca>
2024-09-06 10:50:12 -0400

    Update config.ts

--- config.ts
+++ config.ts
@@ -1,9 +1,9 @@
 import { Config } from "./src/config";
 
 export const defaultConfig: Config = {
-  url: "https://www.builder.io/c/docs/developers",
-  match: "https://www.builder.io/c/docs/**",
-  maxPagesToCrawl: 50,
-  outputFileName: "output.json",
-  maxTokens: 2000000,
+  url: "https://tailwindcss.com/docs",
+  match: "https://tailwindcss.com/docs/**/*",
+  selector: "body",
+  maxPagesToCrawl: 3500,
+  outputFileName: "tailwindcss.com.json",
 };
